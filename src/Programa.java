4032ac898c70aced3857a8caf8907e36ef28ca18
Vinícius Gonçalves <103079267+goncalves2500@users.noreply.github.com>
2022-06-01 00:04:50 -0300

    Add files via upload

---
+++ src/Programa.java
@@ -0,0 +1,31 @@
+public class Programa{
+    public static void main(String[] args) {
+        Conta[] contas = new Conta[2];
+        
+        Conta contaMaria = new Conta();
+        contaMaria.setBanco("Banco das Chaves");
+        contaMaria.setAgencia("013098756");
+        contaMaria.setTitular("Maria das Neves");
+        contaMaria.setSaldo(5);
+        contas[0] = contaMaria;
+
+        Conta contaRamon = new Conta();
+        contaRamon.setBanco("Banco Grêmio");
+        contaRamon.setAgencia("013765290");
+        contaRamon.setTitular("Dom Ramon");
+        contaRamon.setSaldo(0);
+        contas[1] = contaRamon;
+        
+        contaMaria.transfere(5, contaRamon);
+        
+
+        // for(int i = 0; i < contas.length; i++){
+        //     System.out.println("Banco: " + contas[i].banco);
+        //     System.out.println("Agência: " + contas[i].agencia);
+        //     System.out.println("Titular: " + contas[i].titular);
+        //     System.out.println("Saldo: " + contas[i].saldo + "\n");
+        // }
+        
+
+    }
+  }
